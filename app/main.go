cbea7340e21715649dea1f3299bf0fe766e69ea0
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-05-10 20:35:42 +0900

    wip

--- app/main.go
+++ app/main.go
@@ -4,11 +4,38 @@ import (
 	"github.com/kuniyoshi/sgs_image/scenario"
 )
 
+type Vector3 struct {
+	x float64
+	y float64
+	z float64
+}
+
+type Camera struct {
+	positoin  Vector3
+	directoin Vector3
+}
+
+type Scene struct {
+	camera *Camera
+}
+
+func (scene *Scene) sync(snapshot scenario.Snapshot) {
+
+}
+
 func main() {
+	scene := Scene{
+		camera: &Camera{},
+	}
+
 	scenario.Begin()
 
 	for !scenario.IsEnd() {
-		scenario.Progress()
+		snapshot := scenario.Progress()
+
+		go func() {
+			defer scene.sync(snapshot)
+		}()
 	}
 
 	scenario.End()
--- scenario/scenario.go
+++ scenario/scenario.go
@@ -4,6 +4,21 @@ import "log"
 
 var count = 0
 
+type Vector3 struct {
+	x float64
+	y float64
+	z float64
+}
+
+type Camera struct {
+	positoin  Vector3
+	direction Vector3
+}
+
+type Snapshot interface {
+	Camera() Camera
+}
+
 func Begin() {
 	log.Println("Begin")
 }
@@ -13,8 +28,9 @@ func IsEnd() bool {
 	return count > 2
 }
 
-func Progress() {
+func Progress() Snapshot {
 	log.Println("Progress")
+	return Snapshot{}
 }
 
 func End() {
